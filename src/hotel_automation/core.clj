3015c9bd45c2aa71b3c1b3204d73c6b660e83e5b
Mayur Pandey <mayur.pandey@kroo.com>
2021-06-19 11:22:42 +0530

    print state implementation

--- src/hotel_automation/core.clj
+++ src/hotel_automation/core.clj
@@ -18,7 +18,29 @@
      :main-corridors (construct-main-corridors main-corridors)
      :sub-corridors (construct-sub-corridors sub-corridors)}))
 
+(defn- boolean->ON-OFF [bool]
+  (if bool
+    "ON" "OFF"))
+
+(defn- print-corridor [type {:keys [id light AC]}]
+  (println  (format "%s corridor %s Light %s: %s AC: %s"
+              type id id
+              (boolean->ON-OFF light)
+              (boolean->ON-OFF AC))))
+
+(defn- print-state [state]
+  (doseq [floor (:floors state)]
+    (println "Floor " (:id floor))
+
+    (doseq [main-corridor (:main-corridors floor)]
+      (print-corridor "Main" main-corridor ))
+
+    (doseq [sub-corridor (:sub-corridors floor)]
+      (print-corridor "Sub" sub-corridor))))
+
 (defn construct-initial-state [floors main-corridor-per-floor sub-corridor-per-floor]
-  {:floors (construct-floor floors
+  (let [default-state {:floors (construct-floor floors
              main-corridor-per-floor
-             sub-corridor-per-floor)})
+             sub-corridor-per-floor)}]
+    (print-state default-state)
+    default-state))
