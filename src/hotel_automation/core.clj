3d961ce6bb7ec528b0582dba60b2ec26379f4ac8
Mayur Pandey <mayur.pandey@kroo.com>
2021-06-19 10:35:00 +0530

    default state setup implementation

--- src/hotel_automation/core.clj
+++ src/hotel_automation/core.clj
@@ -1,4 +1,24 @@
 (ns hotel-automation.core)
 
+(defn construct-main-corridors [main-corridors]
+  (for [id (range 1 (inc main-corridors))]
+    {:id id
+     :light true
+     :AC true}))
+
+(defn construct-sub-corridors [sub-corridors]
+  (for [id (range 1 (inc sub-corridors))]
+    {:id id
+     :light false
+     :AC true}))
+
+(defn construct-floor [floors main-corridors sub-corridors]
+  (for [floor-id (range 1 (inc floors))]
+    {:id floor-id
+     :main-corridors (construct-main-corridors main-corridors)
+     :sub-corridors (construct-sub-corridors sub-corridors)}))
+
 (defn construct-initial-state [floors main-corridor-per-floor sub-corridor-per-floor]
-  {})
+  {:floors (construct-floor floors
+             main-corridor-per-floor
+             sub-corridor-per-floor)})
--- test/hotel_automation/core_test.clj
+++ test/hotel_automation/core_test.clj
@@ -7,25 +7,15 @@
     (let [floors 2
           main-corridor-per-floor 1
           sub-corridor-per-floor 2
-          expected-state {:floors [{:id 1
-                                   :main-corridors [{:id 1
-                                                     :light true
-                                                     :AC true}]
-                                    :sub-corridors [{:id 1
-                                                     :light false
-                                                     :AC true}
-                                                    {:id 2
-                                                     :light false
-                                                     :AC true}]}
-                                   {:id 2
-                                    :main-corridors [{:id 1
-                                                      :light true
-                                                      :AC true}]
-                                    :sub-corridors [{:id 1
-                                                     :light false
-                                                     :AC true}
-                                                    {:id 2
-                                                     :light false
-                                                     :AC true}]}]}
-          result (construct-initial-state floors main-corridor-per-floor sub-corridor-per-floor)]
-      (is (= 2 (count (:floor result)))))))
+          result (construct-initial-state floors main-corridor-per-floor sub-corridor-per-floor)
+          first-floor (first (:floors result))
+          main-corridors (:main-corridors first-floor)
+          sub-corridors (:sub-corridors first-floor)
+          ]
+      (is (= 2 (count (:floors result))))
+      (is (= 1 (count main-corridors)))
+      (is (= 2 (count sub-corridors)))
+
+      (is (= {:id 1 :light true :AC true} (first main-corridors)))
+      (is (= {:id 1 :light false :AC true} (first sub-corridors)))
+      (is (= {:id 2 :light false :AC true} (second sub-corridors))))))
